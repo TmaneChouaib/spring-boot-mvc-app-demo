5d56c1baf4fc21275c3f726cffe2139918786ef1
TmaneChouaib <chouaib.tmane@hotmail.com>
2024-02-22 15:48:51 +0100

    first commit

---
+++ src/main/java/com/tmane/springbootmvcdemo/SpringBootMvcDemoApplication.java
@@ -0,0 +1,13 @@
+package com.tmane.springbootmvcdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMvcDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMvcDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tmane/springbootmvcdemo/SpringBootMvcDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tmane.springbootmvcdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMvcDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
