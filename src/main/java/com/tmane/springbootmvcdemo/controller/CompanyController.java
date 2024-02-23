227c510a2b64b91d73e12385d9925506a8c264f7
TmaneChouaib <chouaib.tmane@hotmail.com>
2024-02-23 22:54:47 +0100

    Added methods for adding a new company and saving it with redirection after saving

--- src/main/java/com/tmane/springbootmvcdemo/controller/CompanyController.java
+++ src/main/java/com/tmane/springbootmvcdemo/controller/CompanyController.java
@@ -1,21 +1,21 @@
 package com.tmane.springbootmvcdemo.controller;
 
 import com.tmane.springbootmvcdemo.entity.Company;
+import com.tmane.springbootmvcdemo.repository.CompanyRepository;
 import com.tmane.springbootmvcdemo.service.CompanyService;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/companies")
 @AllArgsConstructor
 public class CompanyController {
     private CompanyService companyService;
+    private final CompanyRepository companyRepository;
 
     @GetMapping
     public String getAllCompanies(Model model,
@@ -30,6 +30,20 @@ public class CompanyController {
         model.addAttribute("keyWord", keyWord);
 
         return "companies";
+    }
+
+    @GetMapping("/new")
+    public String AddCompany(@ModelAttribute("company") Company company) {
+        return "AddCompany";
+    }
+
+    @PutMapping
+    public String SaveCompany(@ModelAttribute("company") Company company,
+                              @RequestParam(name = "page", defaultValue = "0") int page,
+                              @RequestParam(name = "keyword", defaultValue = "") String keyWord) {
+
+        companyRepository.save(company);
+        return "redirect:/companies?page=" + page + "&keyword=" + keyWord;
     }
 
 }
