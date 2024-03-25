ef579f0bdf2781b84eeb678a865f27a52198510c
TmaneChouaib <chouaib.tmane@hotmail.com>
2024-03-25 12:19:41 +0000

    Refactored the service layer to include exception handling for retrieving company by ID

--- src/main/java/com/tmane/springbootmvcdemo/service/CompanyService.java
+++ src/main/java/com/tmane/springbootmvcdemo/service/CompanyService.java
@@ -1,6 +1,7 @@
 package com.tmane.springbootmvcdemo.service;
 
 import com.tmane.springbootmvcdemo.dto.CompanyDTO;
+import com.tmane.springbootmvcdemo.exception.CompanyNoSuchElementException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
@@ -10,7 +11,7 @@ public interface CompanyService {
 
     Page<CompanyDTO> findPaginatedCompaniesByName(String name, Pageable pageable);
 
-    CompanyDTO findCompanyById(Long id);
+    CompanyDTO findCompanyById(Long id) throws CompanyNoSuchElementException;
 
     CompanyDTO saveCompany(CompanyDTO companyDTO);
 
--- src/main/java/com/tmane/springbootmvcdemo/service/Impl/CompanyServiceImpl.java
+++ src/main/java/com/tmane/springbootmvcdemo/service/Impl/CompanyServiceImpl.java
@@ -2,20 +2,26 @@ package com.tmane.springbootmvcdemo.service.Impl;
 
 import com.tmane.springbootmvcdemo.dto.CompanyDTO;
 import com.tmane.springbootmvcdemo.entity.Company;
+import com.tmane.springbootmvcdemo.exception.CompanyNoSuchElementException;
 import com.tmane.springbootmvcdemo.mapper.CompanyMapper;
 import com.tmane.springbootmvcdemo.repository.CompanyRepository;
 import com.tmane.springbootmvcdemo.service.CompanyService;
 import lombok.AllArgsConstructor;
+import org.springframework.context.MessageSource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.Locale;
+import java.util.NoSuchElementException;
 
 @Service
 @AllArgsConstructor
 public class CompanyServiceImpl implements CompanyService {
     private CompanyRepository companyRepository;
     private CompanyMapper companyMapper;
+    private MessageSource messageSource;
 
     @Override
     public Page<CompanyDTO> findPaginatedCompanies(int pageNum, int pageSize) {
@@ -33,11 +39,14 @@ public class CompanyServiceImpl implements CompanyService {
 
     @Override
     public CompanyDTO findCompanyById(Long id) {
-
-        Company company = companyRepository.findById(id).get();
-        return companyMapper.mapToCompanyDTO(company);
+        try {
+            Company company = companyRepository.findById(id).get();
+            return companyMapper.mapToCompanyDTO(company);
+        } catch (NoSuchElementException exception) {
+            String message = messageSource.getMessage("entity.notfound", new Object[]{id}, Locale.getDefault());
+            throw new CompanyNoSuchElementException(message, id);
+        }
     }
-
 
     @Override
     public CompanyDTO saveCompany(CompanyDTO companyDTO) {
