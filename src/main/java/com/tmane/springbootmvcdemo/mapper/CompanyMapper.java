86ffc8a65e7e467e804a90bb7dd03fa123b30f97
TmaneChouaib <chouaib.tmane@hotmail.com>
2024-03-25 09:06:39 +0000

    Added Company Mapper

---
+++ src/main/java/com/tmane/springbootmvcdemo/mapper/CompanyMapper.java
@@ -0,0 +1,53 @@
+package com.tmane.springbootmvcdemo.mapper;
+
+import com.tmane.springbootmvcdemo.dto.CompanyDTO;
+import com.tmane.springbootmvcdemo.entity.Company;
+import com.tmane.springbootmvcdemo.enums.Sector;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+
+@Component
+public class CompanyMapper {
+    public CompanyDTO mapToCompanyDTO(Company company) {
+        CompanyDTO companyDTO=new CompanyDTO();
+        companyDTO.setId(companyDTO.getId());
+        companyDTO.setName(company.getName());
+        companyDTO.setAddress(company.getAddress());
+        companyDTO.setCountry(company.getCountry());
+        companyDTO.setCity(company.getCity());
+        companyDTO.setPostalCode(company.getPostalCode());
+        companyDTO.setCEO(company.getCEO());
+        companyDTO.setPhoneNumber(company.getPhoneNumber());
+        companyDTO.setEmail(company.getEmail());
+        companyDTO.setFoundationDate(company.getFoundationDate());
+        companyDTO.setRevenue(company.getRevenue());
+        companyDTO.setNumberOfEmployees(company.getNumberOfEmployees());
+        companyDTO.setFiscalCode(company.getFiscalCode());
+        companyDTO.setSector(company.getSector());
+
+        return companyDTO;
+    }
+
+    public Company mapToCompany(CompanyDTO companyDTO){
+        Company company=new Company();
+
+        company.setName(companyDTO.getName());
+        company.setAddress(companyDTO.getAddress());
+        company.setCountry(companyDTO.getCountry());
+        company.setCity(companyDTO.getCity());
+        company.setPostalCode(companyDTO.getPostalCode());
+        company.setCEO(companyDTO.getCEO());
+        company.setPhoneNumber(companyDTO.getPhoneNumber());
+        company.setEmail(companyDTO.getEmail());
+        company.setFoundationDate(companyDTO.getFoundationDate());
+        company.setRevenue(companyDTO.getRevenue());
+        company.setNumberOfEmployees(companyDTO.getNumberOfEmployees());
+        company.setFiscalCode(companyDTO.getFiscalCode());
+        company.setSector(companyDTO.getSector());
+
+        return company;
+    }
+}
