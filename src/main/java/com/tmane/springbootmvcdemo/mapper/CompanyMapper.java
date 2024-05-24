6ad8021f27dd5269f1cb1ed2e2d2e386c7815e3f
TmaneChouaib <chouaib.tmane@hotmail.com>
2024-05-24 15:47:46 +0100

    Added model mapper

--- src/main/java/com/tmane/springbootmvcdemo/mapper/CompanyMapper.java
+++ src/main/java/com/tmane/springbootmvcdemo/mapper/CompanyMapper.java
@@ -3,48 +3,19 @@ package com.tmane.springbootmvcdemo.mapper;
 import com.tmane.springbootmvcdemo.dto.CompanyDTO;
 import com.tmane.springbootmvcdemo.entity.Company;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
 @Component
-public class    CompanyMapper {
+public class CompanyMapper {
+
+    ModelMapper modelMapper = new ModelMapper();
+
     public CompanyDTO mapToCompanyDTO(Company company) {
-        CompanyDTO companyDTO=new CompanyDTO();
-
-        companyDTO.setId(company.getId());
-        companyDTO.setName(company.getName());
-        companyDTO.setAddress(company.getAddress());
-        companyDTO.setCountry(company.getCountry());
-        companyDTO.setCity(company.getCity());
-        companyDTO.setPostalCode(company.getPostalCode());
-        companyDTO.setCEO(company.getCEO());
-        companyDTO.setPhoneNumber(company.getPhoneNumber());
-        companyDTO.setEmail(company.getEmail());
-        companyDTO.setFoundationDate(company.getFoundationDate());
-        companyDTO.setRevenue(company.getRevenue());
-        companyDTO.setNumberOfEmployees(company.getNumberOfEmployees());
-        companyDTO.setFiscalCode(company.getFiscalCode());
-        companyDTO.setSector(company.getSector());
-
-        return companyDTO;
+        return modelMapper.map(company, CompanyDTO.class);
     }
 
-    public Company mapToCompany(CompanyDTO companyDTO){
-        Company company=new Company();
-
-        company.setName(companyDTO.getName());
-        company.setAddress(companyDTO.getAddress());
-        company.setCountry(companyDTO.getCountry());
-        company.setCity(companyDTO.getCity());
-        company.setPostalCode(companyDTO.getPostalCode());
-        company.setCEO(companyDTO.getCEO());
-        company.setPhoneNumber(companyDTO.getPhoneNumber());
-        company.setEmail(companyDTO.getEmail());
-        company.setFoundationDate(companyDTO.getFoundationDate());
-        company.setRevenue(companyDTO.getRevenue());
-        company.setNumberOfEmployees(companyDTO.getNumberOfEmployees());
-        company.setFiscalCode(companyDTO.getFiscalCode());
-        company.setSector(companyDTO.getSector());
-
-        return company;
+    public Company mapToCompany(CompanyDTO companyDTO) {
+        return modelMapper.map(companyDTO, Company.class);
     }
 }
